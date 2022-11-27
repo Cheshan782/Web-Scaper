f86ad44000782286c09c8ef23573748066e45173
CheshanCoder <88943662+Cheshan782@users.noreply.github.com>
2022-11-27 17:48:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 app.use(cors())
 
 
-const url = 'https://www.vistek.ca/category/cameras?hierarchicalMenu%5Bcategories.lvl0%5D%5B0%5D=Cameras&range%5BunitPrice%5D=0%3A'
+const url = 'https://www.theguardian.com/'
 //app.METHOD(PATH, HANDLER)
 
 //app.get('/', function (req, res) {
